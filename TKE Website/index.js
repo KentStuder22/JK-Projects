556c7f44c11f5483edfc840811d85353f0479079
jamtip <jamesc.tipton@gmail.com>
2021-11-18 17:44:01 -0600

    added slideshow

---
+++ TKE Website/index.js
@@ -0,0 +1,22 @@
+
+var slides = [
+    "./slideshow/2019Decks.PNG",
+    "./slideshow/fenceChill.PNG",
+    "./slideshow/frontHouse.PNG",
+    "./slideshow/hoco2017Skit.PNG"
+];
+
+var slideIndex = 0;
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  if (n > slides.length-1) { slideIndex = 0; }
+  if (n < 0) { slideIndex = slides.length-1; }
+  console.log(n);
+
+  document.getElementById("display").setAttribute("src",slides[slideIndex]);
+}
