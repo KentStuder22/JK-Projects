6613ff682b583de2c6b91c97a22b4c243ffe0c0a
KentStuder22 <77794005+KentStuder22@users.noreply.github.com>
2021-11-20 14:54:00 -0600

    universalizing photos in main slide

--- TKE Website/index.js
+++ TKE Website/index.js
@@ -1,10 +1,21 @@
 
 var slides = [
     "./slideshow/2019Decks.PNG",
+    "./slideshow/AEClass.JPG",
+    "./slideshow/baseketballAction.JPG",
+    "./slideshow/basketballJBS.JPG",
+    "./slideshow/DaggerMuck.JPG",
     "./slideshow/execSki.PNG",
     "./slideshow/fenceChill.PNG",
     "./slideshow/frontHouse.PNG",
-    "./slideshow/hoco2017Skit.PNG"
+    "./slideshow/hoco2017Skit.PNG",
+    "./slideshow/Hoco2021.JPG",
+    "./slideshow/houseChill.JPG",
+    "./slideshow/kdPhilWin.JPG",
+    "./slideshow/kiddyPoolChill.JPG",
+    "./slideshow/MomsWeekend2019.JPG",
+    "./slideshow/penguinChill.JPG",
+    "./slideshow/Rush2019.JPG"
 ];
 
 var slideIndex = 0;
