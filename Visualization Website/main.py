5910e25d89e59446dc512780d5e6c3bd813e50e2
KentStuder22 <77794005+KentStuder22@users.noreply.github.com>
2022-01-27 10:04:56 -0600

    work on formatting svg and connecting mongodb

--- Visualization Website/main.py
+++ Visualization Website/main.py
@@ -9,7 +9,6 @@ import bson
 import pprint
 
 client = pymongo.MongoClient("mongodb://james:IYT7i6rfTR&%25R*&@119.45.163.114:27017/TweetScraper")
-
 db = client['TweetScraper']
 collection = db['tweetday']
 
@@ -20,30 +19,32 @@ class MainHandler(tornado.web.RequestHandler):
         stop_at = 40_000
 
         for tweet in collection.find():
-            if tweet['is_labeled'] and ['label_result']: # only get labeled data
-                tweets.append(json.dumps(tweet))
-                if len(tweets) == stop_at: break
+            tweets.append(json.dumps(tweet))
+            if len(tweets) == stop_at: break
 
-        self.render("index.html", tweets=tornado.escape.json_encode(tweets))    
+        self.render("templates/index.html", tweets=tornado.escape.json_encode(tweets))    
 
 class TrendHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello Worlds")
 
-class Application(tornado.web.Application):
-    def __init__(self):
-        handlers = [
-            (r"/", MainHandler),
-            (r"/trends", TrendHandler),
-        ]
+def setup_app():
 
-        settings = {
-            "debug": True,
-            "static_path": os.path.join(os.path.dirname(__file__), "static")
-        }
-        tornado.web.Application.__init__(self, handlers, **settings)
+    settings = {
+        "static_path": os.path.join(os.path.dirname(__file__), "static")
+    }
+    handlers = [
+        (r'/', MainHandler),
+        (r"/trends", TrendHandler),
+    ]
+    return tornado.web.Application(
+        handlers=handlers,
+        **settings,
+        db = db
+    )
 
 if __name__ == '__main__':
-    application = Application()
+    application = setup_app()
     application.listen(8888)
+    print("The server is listening on port {8888}\n")
     tornado.ioloop.IOLoop.current().start()
