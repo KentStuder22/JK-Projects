96de436efe4e2be451527f24845bdb74e393bab9
jamtip <jctppc@umsystem.edu>
2022-01-28 02:24:44 -0600

    got line graph working with data, takes a minute or two at 10k data items

--- Visualization Website/static/main.js
+++ Visualization Website/static/main.js
@@ -36,14 +36,38 @@ function loaded(str) {
     
     var pos_cases = 0;
     //  data processing
+
+    var jan=0; var feb=0; var mar=0; var apr=0; var may=0; var jun=0;
+    var jul=0; var aug=0; var sep=0; var oct=0; var nov=0; var dec=0;
+
     for(var i=0; i<tweets.length; i++) {
         // console.log(tweets[i]);
         tweets[i][0] = tweets[i][0].split(",");
 
         if(tweets[i][1] === true) {
             pos_cases += 1;
+        
+            // tried Datetime but it was terrible
+            // tweets[i][0][1] = Date.parse(tweets[i][0][1]);
+            // console.log(tweets[i][0][1].slice(1,5)); // year
+            // console.log(tweets[i][0][1].slice(6,8)); // month
+            
+            switch(tweets[i][0][1].slice(6,8)) {
+                case '01': jan++;
+                case '02': feb++;
+                case '03': mar++;
+                case '04': apr++;
+                case '05': may++;
+                case '06': jun++;
+                case '07': jul++;
+                case '08': aug++;
+                case '09': sep++;
+                case '10': oct++;
+                case '11': nov++;
+                case '12': dec++;
+            }
         }
-
+        
     }
     var neg_cases = tweets.length - pos_cases;
 
@@ -75,7 +99,7 @@ function loaded(str) {
             label: 'COVID-19 Cases',
             backgroundColor: 'rgb(0,0,0)',
             borderColor: 'rgb(255,255,255)',
-            data: [1000, 2000, 3000, 4000, 1500, 3000, 7000, 10000, 3000, 7500, 8000, 3500],
+            data: [jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec],
         }]
     };
     const config = {
