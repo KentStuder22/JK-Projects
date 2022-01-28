21e43b6b5200f94924448333d2d428b6b2e19543
jamtip <jctppc@umsystem.edu>
2022-01-27 22:52:05 -0600

    got state tooltip working, minor commenting and html formatting

--- Visualization Website/static/main.js
+++ Visualization Website/static/main.js
@@ -1,6 +1,17 @@
 
-function state_highlight(element) {
-    console.log(element.firstElementChild.innerHTML);
-    element.innerHTML = '<span>' + element.firstElementChild.innerHTML + '</span>';
+// function to present tooltip type div on mouse position based on state its hovering over
+function state_highlight(evt, element) {
+    // console.log(element.firstElementChild.innerHTML);
+    let info = document.getElementById("stateInfo");
+    info.innerHTML = element.firstElementChild.innerHTML;
+    info.style.display = 'block';
+    info.style.left = evt.pageX + 10 + 'px';
+    info.style.top = evt.pageY + 10 + 'px';
+}
+
+// clears tooltip div when mouse leaves map area
+function clear_highlight() {
+    let info = document.getElementById("stateInfo");
+    info.style.display = "none";
 }
 
